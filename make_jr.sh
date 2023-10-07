d3c4ded69abebb722bda82c9ffe0bc7e392ca6bb
scttgs0 <scttgs0@gmail.com>
2023-10-07 01:49:57 -0500

    updated build script

--- make_jr.sh
+++ make_jr.sh
@@ -1,8 +1,12 @@
 
+mkdir -p obj/
+
+# -------------------------------------
+
 64tass  --m65c02 \
         --flat \
         --nostart \
-        -o actionJR.bin \
-        --list=actionJR.lst \
-        --labels=actionJR.lbl \
+        -o obj/actionJR.bin \
+        --list=obj/actionJR.lst \
+        --labels=obj/actionJR.lbl \
         action.asm
