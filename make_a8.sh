a683aa591cdd43c97d08f3e34a050077f5c36368
scttgs0 <scttgs0@gmail.com>
2024-11-30 23:52:13 -0600

    refine build script

--- make_a8.sh
+++ make_a8.sh
@@ -5,7 +5,8 @@ mkdir -p obj/
 
 64tass  --m65xx \
         --atari-xex \
-        -b \
+        --nostart \
+        -D ZAPRAM=1 \
         -o obj/action.rom \
         --list=obj/action_rom.lst \
         --labels=obj/action_rom.lbl \
